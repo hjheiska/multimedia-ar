ab868ccbf9b4ee2f22ab8df89e5a2d60df690a66
Heikki Heiskanen <heikki.j.heiskanen@aalto.fi>
2014-03-25 22:50:21 +0200

    Projektin pohja

---
+++ AR-Project/Assets/Omat assetit/Skriptit/ChangeDimensionsWithKeybaord.js
@@ -0,0 +1,45 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+	
+	
+	if(Input.GetKey ("a")) {
+		transform.position.x += 0.1;
+	}
+	if(Input.GetKey ("d")) {
+		transform.position.x -= 0.1;
+	}
+	if(Input.GetKey ("w")) {
+		transform.position.z += 0.1;
+	}
+	if(Input.GetKey ("s")) {
+		transform.position.z -= 0.1;
+	}
+	if(Input.GetKey ("q")) {
+		transform.localScale.x += 0.1;
+	}
+	
+	if(Input.GetKey ("z")) {
+		transform.position.y += 0.1;
+	}
+	if(Input.GetKey ("x")) {
+		transform.position.y -= 0.1;
+	}
+	
+	if(Input.GetKey ("e")) {
+		transform.localScale.x -= 0.1;
+	}
+	
+	if(Input.GetKey ("r")) {
+		transform.localScale.z += 0.1;
+	}
+	if(Input.GetKey ("f")) {
+		transform.localScale.z -= 0.1;
+	}
+	
+	
+}
