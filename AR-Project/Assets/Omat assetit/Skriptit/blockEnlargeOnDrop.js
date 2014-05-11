2c658948656931f34f412b8d14d6c0292cc67721
Heikki Heiskanen <heikki.j.heiskanen@aalto.fi>
2014-05-12 01:52:46 +0300

    Some changes

---
+++ AR-Project/Assets/Omat assetit/Skriptit/blockEnlargeOnDrop.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+	if(transform.position.y < 0) {
+		transform.localScale = Vector3(1, 36.71283, 1);
+	}
+}
--- AR-Project/Assets/Omat assetit/Skriptit/respawnPlayer.js
+++ AR-Project/Assets/Omat assetit/Skriptit/respawnPlayer.js
@@ -7,6 +7,6 @@ function Start () {
 function Update () {
 	if(transform.position.y < -100) {
 		rigidbody.velocity = Vector3.zero;
-		transform.position = Vector3(-5, -1, 3);
+		transform.position = Vector3(2.77791, 3.772754, 12.45419);
 	}
 }
